be7230acc2888e318df7b32db7e31c9a8745149c
Babatunde Joseph <babatundejoseph85@gmail.com>
2022-08-07 20:24:32 +0100

    deleted commented file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "components/Header";
-import HomePage from "./pages/Home";
-import PropertyDetailsPage from "./pages/PropertyDetails";
-import { PropertyPage } from "./pages/PropertyPage";
-import LoginPage from "pages/loginPage";
-import SignUpPage from "pages/signUpPage";
-import OrderPage from "pages/OrderPage";
-import AddPropertyPage from "pages/AddPropertyPage";
-import Navbar from './components/Navbar';
 import StudyPage from './pages/StudyPage';
 
 function App() {
